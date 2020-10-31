17480efae38d383ce9727598b2d5a40d2de96a7e
Chris Fogelklou <chris.fogelklou@gmail.com>
2020-10-31 11:25:27 +0100

    wip

--- bash_scripts/build_parallel_20.sh
+++ bash_scripts/build_parallel_20.sh
@@ -2,11 +2,14 @@ scons -j20 platform=android target=release android_arch=armv7
 scons -j20 platform=android target=release android_arch=arm64v8
 scons -j20 platform=android target=release android_arch=x86
 scons -j20 platform=android target=release android_arch=x86_64
+
 scons -j20 platform=android target=release_debug android_arch=armv7
 scons -j20 platform=android target=release_debug android_arch=arm64v8
-
+scons -j20 platform=android target=release_debug android_arch=x86
+scons -j20 platform=android target=release_debug android_arch=x86_64
 
 cd platform/android/java
+
 # On Windows
 #.\gradlew generateGodotTemplates
 # On Linux and macOS
