9e7b4b8a603d79def641ada66127a871476ec818
Chris Fogelklou <chris.fogelklou@gmail.com>
2020-10-31 11:07:02 +0100

    Update scripts for 20 core build.

--- bash_scripts/build_arm.sh
+++ bash_scripts/build_arm.sh
@@ -1,7 +1,13 @@
-scons platform=android target=release android_arch=armv7
-scons platform=android target=release android_arch=arm64v8
+scons -j20 platform=android target=release android_arch=armv7
+scons -j20 platform=android target=release android_arch=arm64v8
+scons -j20 platform=android target=release android_arch=x86
+scons -j20 platform=android target=release android_arch=x86_64
+scons -j20 platform=android target=release_debug android_arch=armv7
+scons -j20 platform=android target=release_debug android_arch=arm64v8
+
+
 cd platform/android/java
 # On Windows
-.\gradlew generateGodotTemplates
+#.\gradlew generateGodotTemplates
 # On Linux and macOS
 ./gradlew generateGodotTemplates
